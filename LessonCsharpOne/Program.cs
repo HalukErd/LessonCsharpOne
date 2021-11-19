6322921978a6814ea009fa569339272468573c48
HalukErd <erd.haluk@gmail.com>
2021-11-20 02:40:32 +0300

    initial

--- LessonCsharpOne/Program.cs
+++ LessonCsharpOne/Program.cs
@@ -6,34 +6,21 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            int day = 4;
-            switch (day)
+            int i = 0;
+            while (i < 5)
             {
-                case 1:
-                    Console.WriteLine("Monday");
-                    break;
-                case 2:
-                    Console.WriteLine("Tuesday");
-                    break;
-                case 3:
-                    Console.WriteLine("Wednesday");
-                    break;
-                case 4:
-                    Console.WriteLine("Thursday");
-                    break;
-                case 5:
-                    Console.WriteLine("Friday");
-                    break;
-                case 6:
-                    Console.WriteLine("Saturday");
-                    break;
-                case 7:
-                    Console.WriteLine("Sunday");
-                    break;
-                default:
-                    Console.WriteLine("Yanlış Girdiniz!");
-                    break;
+                Console.WriteLine(i);
+                i++;
             }
+
+
+            int j = 0;
+            do
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+            while (j < 5);
         }
     }
 }
