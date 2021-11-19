b6a31eb8bfb9d7e7642f294cbe29cae28a32c19f
HalukErd <erd.haluk@gmail.com>
2021-11-20 02:10:28 +0300

    initial

--- LessonCsharpOne/Program.cs
+++ LessonCsharpOne/Program.cs
@@ -20,6 +20,18 @@ namespace HelloWorld
                 Console.WriteLine("x is greater than y");
             }
 
+            int time = 20;
+            if (time < 18)
+            {
+                Console.WriteLine("Good day.");
+            }
+            else
+            {
+                Console.WriteLine("Good evening.");
+            }
+
+            string result = (time < 18) ? "Good day." : "Good evening.";
+            Console.WriteLine(result);
         }
     }
 }
