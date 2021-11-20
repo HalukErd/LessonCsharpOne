0659049308471b5b1484744935c87059c3b027a4
HalukErd <erd.haluk@gmail.com>
2021-11-20 03:33:21 +0300

    random

--- LessonCsharpOne/Program.cs
+++ LessonCsharpOne/Program.cs
@@ -51,6 +51,29 @@ namespace HelloWorld
             cars6 = new string[] { "Volvo", "BMW", "Ford" };
             // Add values without using new (this will cause an error)
             // ERROR cars6 = { "Volvo", "BMW", "Ford"};
+
+
+            Random rnd = new Random();
+            string[] malePetNames = { "Rufus", "Bear", "Dakota", "Fido",
+                          "Vanya", "Samuel", "Koani", "Volodya",
+                          "Prince", "Yiska" };
+            string[] femalePetNames = { "Maggie", "Penny", "Saya", "Princess",
+                            "Abby", "Laila", "Sadie", "Olivia",
+                            "Starlight", "Talla" };
+
+            // Generate random indexes for pet names.
+            int mIndex = rnd.Next(malePetNames.Length);
+            int fIndex = rnd.Next(femalePetNames.Length);
+
+            // Display the result.
+            Console.WriteLine("Suggested pet name of the day: ");
+            Console.WriteLine("   For a male:     {0}", malePetNames[mIndex]);
+            Console.WriteLine("   For a female:   {0}", femalePetNames[fIndex]);
+
+            // The example displays output similar to the following:
+            //       Suggested pet name of the day:
+            //          For a male:     Koani
+            //          For a female:   Maggie
         }
     }
 }
