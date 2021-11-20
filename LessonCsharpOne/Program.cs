4fe1ef4333de6d82407c89133e5a2864296e2a24
HalukErd <erd.haluk@gmail.com>
2021-11-20 17:14:45 +0300

    initial

--- LessonCsharpOne/Program.cs
+++ LessonCsharpOne/Program.cs
@@ -1,53 +1,52 @@
-﻿//String userNameInDb = "defneSa";
-//String passwordInDb = "asd421";
-//Console.Write("Enter your username: ");
-//string userName = Console.ReadLine();
-//Console.Write("Enter your password: ");
-//string password = Console.ReadLine();
-//string resultDb = $"In db information is: {userNameInDb} {passwordInDb}";
-//string result = $"Your information is: {userName} {password}";
+﻿using System;
 
-//bool equals = userName.ToLower() == userNameInDb.ToLower();
-//Console.WriteLine(equals);
-
-//Console.WriteLine();
-//Console.WriteLine();
-//Console.WriteLine(resultDb);
-//Console.WriteLine(result);
-
-//string hello = "Hello World, Defne";
-
-//int index = hello.IndexOf(' ');
-//Console.WriteLine(index);   
-//string result = hello.Substring(index + 1);
-//index = result.IndexOf("Def");
-//result = result.Substring(index + 1);
-//Console.WriteLine(result);
-
-//string hello = "Hello World, Defne";
-
-//int index = hello.IndexOf("Def");
-//Console.WriteLine(index);
-//string result = hello.Substring(6, 5);
-//Console.WriteLine(result);
-
-String userNameInDb = "defneSa";
-String passwordInDb = "asd421";
-Console.Write("Enter your username: ");
-string userName = Console.ReadLine();
-Console.Write("Enter your password: ");
-userName = Console.ReadLine();
-Console.Write("Enter your password: ");
-string password = Console.ReadLine();
-
-bool equals = userName.ToLower().Equals(userNameInDb.ToLower());
-if (equals)
+namespace HelloWorld
 {
-    Console.WriteLine("Girdiğin username doğru.");
-    Console.WriteLine("Sisteme giriş yaptın");
-}
-Console.WriteLine("İşlemin sonucu 400");
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.Write("Enter your first name: ");
+            String fName = Console.ReadLine();
+            Console.Write("Enter your last name: ");
+            String lName = Console.ReadLine();
+            Console.WriteLine("Hello " + fName + " " + lName + ", welcome first class.");
 
-// Ekrana ilk sayıyı gir yazacak. İkinci sayıyı gir yazacak.
-// Yapmak istediğin işlemi gir yazacaksın.
-// O seçilen işleme göre matematik işlemini yapıp ekrana yazdır.
+
+            //int -stores integers(whole numbers), without decimals, such as 123 or - 123
+            //double -stores floating point numbers, with decimals, such as 19.99 or - 19.99
+            //char -stores single characters, such as 'a' or 'B'.Char values are surrounded by single quotes
+            //string -stores text, such as "Hello World".String values are surrounded by double quotes
+            //bool -stores values with two states: true or false
+
+            int myNum3 = 5;
+            double myDoubleNum = 5.99D;
+            char myLetter = 'D';
+            bool myBool = true;
+            string myText = "Hello";
+
+            string name = "John";
+            Console.WriteLine(name);
+
+            int myNum2 = 15;
+            Console.WriteLine(myNum2);
+
+            int myNum;
+            myNum = 15;
+            Console.WriteLine(myNum);
+
+            const double pi = 3.14;
+            //pi = 3;
+
+            //Names can contain letters, digits and the underscore character(_)
+            //Names must begin with a letter
+            //Names should start with a lowercase letter and it cannot contain whitespace
+            //Names are case sensitive("myVar" and "myvar" are different variables)
+            //Reserved words(like C# keywords, such as int or double) cannot be used as names
+
+
+
+        }
+    }
+}
