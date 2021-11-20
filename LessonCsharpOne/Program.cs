47bcf887467207dca1bb6c9d2538d97be04d65e3
HalukErd <erd.haluk@gmail.com>
2021-11-20 04:51:16 +0300

    initial

--- LessonCsharpOne/Program.cs
+++ LessonCsharpOne/Program.cs
@@ -6,24 +6,38 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(factorial(5));
-        }
-
-        static int factorial(int number)
-        {
-            int result = 1;
-            for (int i = 1; i <= number; i++)
+            int i;
+            Console.Write("Enter the Number of values to be Sort : ");
+            // read the string value (by default) and convert it in to integer  
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[n]; // Array Declaration in C#  
+            //Reading the values one by one  
+            for (i = 1; i <= n; i++)
             {
-                result *= i;
+                Console.Write("Enter the No " + i + ":");
+                // read the string value (by default) and convert it in to integer  
+                a[i] = Convert.ToInt16(Console.ReadLine());
             }
-            return result;
+
+            for (i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n - 1; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        int temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+            //Display the Ascending values one by one  
+            Console.Write("Ascending Sort : ");
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
         }
 
-        static int factorialRecursive(int number)
-        {
-            if(number == 1)
-                return 1;
-            return number * factorial(number - 1);
-        }
     }
 }
