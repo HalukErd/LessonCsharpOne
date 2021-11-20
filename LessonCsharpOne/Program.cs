4d2c1ccdbf12e258b8338a24a4388975d41892cb
HalukErd <erd.haluk@gmail.com>
2021-11-20 03:14:50 +0300

    initial

--- LessonCsharpOne/Program.cs
+++ LessonCsharpOne/Program.cs
@@ -7,50 +7,41 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
-            int[] myNum = { 10, 20, 30, 40 };
-            Console.WriteLine(cars[0]);
-            cars[0] = "Opel";
-            Console.WriteLine(cars[0]);
+            MyMethod();
+            string fname = "Haluk";
+            sayHiTo(fname);
+            int result = sumTwoNumbers(123, 462);
+            Console.WriteLine(result);
+            sayHiTo(fname, "Erd");
+            sayHiTo();
+            sayHiTo(lname: "Erd", fname: fname);
+        }
 
-            // Sort an int
-            int[] myNumbers = { 5, 1, 8, 9 };
-            Array.Sort(myNumbers);
-            foreach (int i in myNumbers)
-            {
-                Console.WriteLine(i);
-            }
+        static void MyMethod()
+        {
+            Console.WriteLine("I just got executed!");
+        }
 
-            // Sort a string
-            Array.Sort(cars);
-            foreach (string i in cars)
-            {
-                Console.WriteLine(i);
-            }
+        static void sayHiTo(string fname)
+        {
+            Console.WriteLine("Hello " + fname);
+        }
 
-            Console.WriteLine(myNumbers.Max());  // returns the largest value
-            Console.WriteLine(myNumbers.Min());  // returns the smallest value
-            Console.WriteLine(myNumbers.Sum());
+        static void sayHiTo(string fname = "Stranger", string lname = "")
+        {
+            //string space;
+            Console.WriteLine("Hello " + fname + " " + lname);
+        }
 
-            // Create an array of four elements, and add values later
-            string[] cars2 = new string[4];
-
-            // Create an array of four elements and add values right away 
-            string[] cars3 = new string[4] { "Volvo", "BMW", "Ford", "Mazda" };
-
-            // Create an array of four elements without specifying the size 
-            string[] cars4 = new string[] { "Volvo", "BMW", "Ford", "Mazda" };
-
-            // Create an array of four elements, omitting the new keyword, and without specifying the size
-            string[] cars5 = { "Volvo", "BMW", "Ford", "Mazda" };
+        static int sumTwoNumbers(int x, int y)
+        {
+            return x + y;
+        }
 
 
-            // Declare an array
-            string[] cars6;
-            // Add values, using new
-            cars6 = new string[] { "Volvo", "BMW", "Ford" };
-            // Add values without using new (this will cause an error)
-            // ERROR cars6 = { "Volvo", "BMW", "Ford"};
+        static double sumTwoNumbers(double x, double y)
+        {
+            return x + y;
         }
     }
 }
