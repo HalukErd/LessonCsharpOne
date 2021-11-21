ebdfacf13b2174ce520f43e19ac2881b5188b318
HalukErd <erd.haluk@gmail.com>
2021-11-21 15:44:38 +0300

    initial

--- LessonCsharpOne/Program.cs
+++ LessonCsharpOne/Program.cs
@@ -6,8 +6,45 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Check Branches");
+            SayHiTo("Defne", "Sağıroğlu");
+            SayHiTo("Haluk", "Erdoğan");
+            SayHiTo("Haluk");
+            SayHiTo("Defne", 15);
+            SayHiTo();
 
+            PrintResult(SumTwoNumbers(123, 125));
+            double result = DivideTwoNumbers(414, 4);
+            PrintResult(result);
+        }
+
+        static void SayHiTo(String name)
+        {
+            Console.WriteLine("Hello {0}", name);
+        }
+
+        static void SayHiTo(String name = "Stranger", String lastName = "")
+        {
+            Console.WriteLine("Hello {0} {1}", name, lastName);
+        }
+
+        static void SayHiTo(String name, int age)
+        {
+            Console.WriteLine("Hello {0}, you are {1}", name, age);
+        }
+
+        static int SumTwoNumbers(int x, int y)
+        {
+            return x + y;
+        }
+
+        static double DivideTwoNumbers(double x, double y)
+        {
+            return x / y;
+        }
+
+        static void PrintResult(double result)
+        {
+            Console.WriteLine("Sonucun: {0}", result);
         }
     }
 }
