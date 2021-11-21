386a1873fab28a8e9da0a3a5d62f02e6155eb8ab
HalukErd <erd.haluk@gmail.com>
2021-11-21 16:02:54 +0300

    initial

--- LessonCsharpOne/Program.cs
+++ LessonCsharpOne/Program.cs
@@ -6,10 +6,20 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(factorial(5));
+            int n = 5;
+            int r = 3;
+            
+            int result = Perm(n, r);
+            Console.WriteLine(result);
         }
 
-        static int factorial(int number)
+        static int Perm(int n, int r)
+        {
+            return Factorial(n) / Factorial(n - r);
+        }
+
+
+        static int Factorial(int number)
         {
             int result = 1;
             for (int i = 1; i <= number; i++)
@@ -19,11 +29,13 @@ namespace HelloWorld
             return result;
         }
 
-        static int factorialRecursive(int number)
+        static int FactorialRecursice(int number)
         {
             if(number == 1)
+            {
                 return 1;
-            return number * factorial(number - 1);
+            }
+            return number * FactorialRecursice(number - 1);
         }
     }
 }
